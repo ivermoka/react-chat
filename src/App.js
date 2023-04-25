070e8ef35ee5e68a56e8e24a7f4ce728a13e88cb
ivermoka <ivermoka@icloud.com>
2023-04-25 13:42:57 +0200

    s

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar";
 import Chat from "./components/Chat"; 
 
 const style = {
-  appContainer: `w-screen bg-black mx-auto text-center`,
+  appContainer: `w-screen h-screen bg-black mx-auto text-center`,
   sectionContainer: `flex flex-col h-[90vh] bg-gray-500 mt-10 shadow-xl border relative`
 }
 
@@ -14,7 +14,7 @@ function App() {
       <section className={style.sectionContainer}>
         {/* Navbar */}
         <Navbar />
-        {/* Chat component */}
+        {user ? <Chat /> : null}
         <Chat />
       </section>
     </div>
